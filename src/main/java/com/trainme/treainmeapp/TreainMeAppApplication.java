6bcea1ecf205db53ba4752c495b7b05808d7058a
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-12-26 16:14:53 +0100

    Initial maven project

---
+++ src/main/java/com/trainme/treainmeapp/TreainMeAppApplication.java
@@ -0,0 +1,13 @@
+package com.trainme.treainmeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreainMeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreainMeAppApplication.class, args);
+	}
+
+}
