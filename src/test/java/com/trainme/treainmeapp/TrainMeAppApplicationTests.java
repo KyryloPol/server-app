031c4848e408db22318f8278c399220b3d319818
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-12-30 22:06:52 +0100

    dev: add tests for web controllers

---
+++ src/test/java/com/trainme/treainmeapp/TrainMeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainme.treainmeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainMeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
